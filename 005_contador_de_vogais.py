2f3c8e0bda3094e57f4267dd93e8b3c44bf75de1
Alex Miranda <alex_miranda@outlook.com.br>
2026-04-06 15:22:21 -0300

    Buscador de vagas

--- 005_contador_de_vogais.py
+++ 005_contador_de_vogais.py
@@ -1,11 +1,11 @@
 # Peça para o usuário digitar uma frase. O seu programa deve contar quantas vogais (a, e, i, o, u) existem nessa frase e exibir o total.
 
-frase = 'Alex Miranda da Silva'#.lower().strip()
-vogais = 'aeiouAEIOU'
+frase = input('Digite um texto: ').lower().strip()
+vogais = 'aeiou'
 contador = 0
 
-for letras in frase:
-    if letras in vogais:
+for letra in frase:
+    if letra in vogais:
         contador = contador + 1
 
-print(contador)
+print('Este texto contem ',contador, ' vogal(is)')
